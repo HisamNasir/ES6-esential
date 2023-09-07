f2e4908e1113240284ab6300640fad3d8793822c
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-09-07 23:22:55 +0500

    Update Task9.js

--- my-app/app/components/Task9.js
+++ my-app/app/components/Task9.js
@@ -16,7 +16,7 @@ export default function Task9() {
     return (
         <div className='w-full h-full flex justify-center items-center'>
             <div className=' text-center py-10 px-20 text-white rounded-xl bg-slate-900'>
-                <h1 className='text-3xl font-thin'>Random 8-Digit Password Generator</h1>
+                <h1 className='text-3xl font-thin'>Random Password</h1>
                 <button className='my-10 py-2 px-4 bg-slate-600 rounded-xl hover:bg-slate-700 hover:animate-pulse'
                     onClick={handleRandomPassword}>
                     Generate Password
