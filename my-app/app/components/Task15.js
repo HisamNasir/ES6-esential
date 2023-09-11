23cee244b397808fe1bf2cb1446c875173bf525d
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-09-11 15:02:39 +0500

    Update Task15.js

--- my-app/app/components/Task15.js
+++ my-app/app/components/Task15.js
@@ -18,24 +18,28 @@ const Task15 = () => {
         image.src = imgPath;
 
         image.onload = () => {
-          resolve(imgPath); };
+          resolve(imgPath);
+        };
         image.onError = (err) => {
-          reject(err); };  });
+          reject(err);
+        };
+      });
     };
-
     const loadImages = async () => {
       try {
         const promises = imgPaths.map((imgPath) => loadImage(imgPath));
         const loaded = await Promise.all(promises);
         setloadedImage(loaded);
-        setLoad(false); }
-        catch (err) {
         setLoad(false);
-        setError('File not found'); } };
+      }
+      catch (err) {
+        setLoad(false);
+        setError('File not found');
+      }
+    };
 
     loadImages();
   }, [imgPaths]);
-
   if (error) {
     return (
       <div className="text-red-500">
@@ -44,15 +48,15 @@ const Task15 = () => {
     );
   }
   if (loading) {
-    return             <div className='absolute z-10 w-full h-full flex justify-center items-center'>
-    <div className='flex space-x-2 text-white'>
+    return <div className='absolute z-10 w-full h-full flex justify-center items-center'>
+      <div className='flex space-x-2 text-white'>
         <div className='animate-pulse'>Loading</div>
         <div className=' animate-spin'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
         </svg>
         </div>
-    </div>
-</div>;
+      </div>
+    </div>;
   }
 
   return (
