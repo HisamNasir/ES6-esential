a23bb4467ddf10e99c36e6743120664e10db98f3
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-09-11 15:00:07 +0500

    Update Task16.js

--- my-app/app/components/Task16.js
+++ my-app/app/components/Task16.js
@@ -5,8 +5,8 @@ const Task16 = () => {
   return (
     <div className=' w-full h-full flex justify-center items-center text-white text-center'>
         <div>
-        <h1 className=' text-4xl font-bold m-2'>The main file</h1>
-        <div className='m-2 text-xl font-light'><Task16child/></div>
+        <h1 className=' text-4xl  font-bold m-2'>The main file</h1>
+        <div className='m-2 text-xl space-x-1 font-light'><Task16child/></div>
         </div>
     </div>
   )
